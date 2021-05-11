470af3f41154f4b058391bf779e4803a480fc7b1
ostmannen <60339614+ostmannen@users.noreply.github.com>
2021-05-11 19:08:03 +0200

    Kompleteringar, gränssnitt, Tryparse, mm

---
+++ slutprojekt/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace slutprojekt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool playGame = true;
+
+            while(playGame)
+            {
+                playGame = age();
+
+                
+            }
+
+            Console.ReadLine();
+        }
+        static bool age()
+        {
+            System.Console.WriteLine("What is your age???");
+
+            string playerInput = Console.ReadLine();
+
+            if (isInt(playerInput))
+            {
+                return false;
+            }
+            else {
+                System.Console.WriteLine(playerInput + " is not a number.");
+            }
+            return true;
+        }
+        static bool isInt(string number)
+        {
+            bool success = int.TryParse(number, out int result);
+
+            return success;
+        }
+    }
+}
